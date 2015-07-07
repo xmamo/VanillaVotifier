b76e7d885c9422c23dc525884988d670c14a16c4
Mamo <morena.matteo@gmail.com>
2015-07-07 12:00:31 +0200

    Fixed: settings don't update on restart.

--- src/com/mamoslab/vanillaVotifier/Utils.java
+++ src/com/mamoslab/vanillaVotifier/Utils.java
@@ -5,7 +5,6 @@ import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
