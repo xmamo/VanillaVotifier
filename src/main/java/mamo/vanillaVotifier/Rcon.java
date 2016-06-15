3e2c14e830ab7beaa481c4b710acbb46758dfdf5
Mamo <morena.matteo@gmail.com>
2016-06-15 18:37:43 +0200

    Command line improvements, rename root package, switch to IntelliJ
    Improved command line:
     * Special characters won't show as "?" anymore;
     * Added "-help" flag to display available startup options (for now
       only "-report-exceptions";
     * Fixed spelling mistake: "avaiable"->"available".
    
    Also, renamed root package "co.virtualdragon" simply to "mamo" and moved
    to IntelliJ instead of NetBeans.

---
+++ src/main/java/mamo/vanillaVotifier/Rcon.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2016  Matteo Morena
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package mamo.vanillaVotifier;
+
+import mamo.vanillaVotifier.Config.RconConfig;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public interface Rcon {
+	RconConfig getRconConfig();
+
+	int getRequestId();
+
+	void connect() throws IOException;
+
+	boolean isConnected();
+
+	Packet logIn() throws UnsupportedEncodingException, IOException;
+
+	Packet sendRequest(Packet request) throws UnsupportedEncodingException, IOException;
+
+	public static interface Packet {
+		int getLength();
+
+		int getRequestId();
+
+		Type getType();
+
+		String getPayload();
+
+		public static enum Type {
+			COMMAND_RESPONSE(0), COMMAND(2), LOG_IN(3);
+
+			private int i;
+
+			private Type(int i) {
+				this.i = i;
+			}
+
+			public int toInt() {
+				return i;
+			}
+
+			public static Type fromInt(int i) {
+				if (i == 0) {
+					return COMMAND_RESPONSE;
+				} else if (i == 2) {
+					return COMMAND;
+				} else if (i == 3) {
+					return LOG_IN;
+				} else {
+					throw new IllegalArgumentException("i has to be equal to 0, 2, or 3!");
+				}
+			}
+		}
+	}
+
+	public static class VanillaVotifierPacket implements Packet {
+		private int length;
+		private int requestId;
+		private Type type;
+		private String payload;
+
+		public VanillaVotifierPacket(int requestId, Type type, String payload) {
+			this(Integer.SIZE / 8 + Integer.SIZE / 8 + payload.length() + Byte.SIZE / 8 * 2, requestId, type, payload);
+		}
+
+		public VanillaVotifierPacket(int length, int requestId, Type type, String payload) {
+			if (type == null) {
+				throw new IllegalArgumentException("type can't be null!");
+			}
+			if (payload == null) {
+				payload = "";
+			}
+			this.length = length;
+			this.requestId = requestId;
+			this.type = type;
+			this.payload = payload;
+		}
+
+		@Override
+		public int getLength() {
+			return length;
+		}
+
+		@Override
+		public int getRequestId() {
+			return requestId;
+		}
+
+		@Override
+		public Type getType() {
+			return type;
+		}
+
+		@Override
+		public String getPayload() {
+			return payload;
+		}
+
+		@Override
+		public String toString() {
+			return length + "\t" + requestId + "\t" + type.toInt() + "\t" + payload;
+		}
+	}
+}
---
+++ src/main/java/mamo/vanillaVotifier/impl/VanillaVotifierVote.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2016  Matteo Morena
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package mamo.vanillaVotifier.impl;
+
+import mamo.vanillaVotifier.Vote;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+public class VanillaVotifierVote implements Vote {
+	private final String serviceName;
+	private final String userName;
+	private final String address;
+	private final String timeStamp;
+
+	public VanillaVotifierVote(String serviceName, String userName, String address) {
+		this(serviceName, userName, address, new Timestamp(Calendar.getInstance().getTime().getTime()).toString());
+	}
+
+	public VanillaVotifierVote(String serviceName, String userName, String address, String timeStamp) {
+		this.serviceName = serviceName;
+		this.userName = userName;
+		this.address = address;
+		this.timeStamp = timeStamp;
+	}
+
+	@Override
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	@Override
+	public String getUserName() {
+		return userName;
+	}
+
+	@Override
+	public String getAddress() {
+		return address;
+	}
+
+	@Override
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+}
